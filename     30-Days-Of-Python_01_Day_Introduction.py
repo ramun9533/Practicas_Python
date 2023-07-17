c2a4551ecf58dc8c20a9bc938b8af1dd3d59c358
William Elí O.L <ramun346416@gmail.com>
2023-07-17 13:54:21 -0600

    practicas de python

---
+++     30-Days-Of-Python_01_Day_Introduction.py
@@ -0,0 +1,20 @@
+print(2 + 3) #Simple suma
+print(3 / 2) #division
+print(3 ** 2) #exponencial
+print(5 % 2) #Modulos
+print(7 // 2) # resultado de la division(floor division operacion)
+print('aserejejaja \n hola crayola')
+print(type(10)) # int
+print(type(10.5)) #float
+print(type(1 + 3j)) #complex
+print(type('aserejejaja \n hola crayola')) #string
+print(type([1, 3, 5])) #list
+print(type({'name': 'Francisco Poja'})) #Dictionary
+#Set
+#Los set en Python son un tipo que permite almacenar 
+# varios elementos y acceder a ellos de una forma muy 
+# similar a las listas pero con ciertas diferencias: 
+# Los elementos de un set son único, lo que significa 
+# que no puede haber elementos duplicados.
+print(type({9.8, 10.5, 1.98})) #Set
+print(type((1.3, 35, 10.1))) #Tupla(Tuple)
